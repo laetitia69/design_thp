b5b1abe086a15a792928412bf5e18aea0e8c3925
laetitia69 <titi-fernandes@hotmail.fr>
2018-08-15 01:18:57 +0200

    tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get static_pages_index_url
-    assert_response :success
-  end
+  
 
+  	test "titre present" do
+    	get root_path
+    	assert_select 'title', "ThpDesign"
+  	end
+
+  	test "navbar presente" do
+    	get home_path
+    	assert_select "nav", 1
+	end
+
+	test "footer presente" do
+    	get home_path
+    	assert_select "footer", 1
+	end
 end
